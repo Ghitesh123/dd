ed195a547307918f026dd1febfff40a40efb5b5b
hitesh <hiteshsuryawanshi70@gmail.com>
2026-03-24 14:05:32 +0530

    this is new

---
+++ src/test/java/com/demo/TestDemo.java
@@ -0,0 +1,21 @@
+package com.demo;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestDemo {
+
+	
+	@Test
+	public void testGoogle() {
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://www.google.com");
+System.out.println("hi hitesh how are you");	
+
+driver.close();
+	}
+}
